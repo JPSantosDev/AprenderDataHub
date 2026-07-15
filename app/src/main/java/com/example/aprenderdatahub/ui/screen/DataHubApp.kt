12c859940085715072d063c965297d3377037e47
Jean Pierre <jp0056392@gmail.com>
2026-07-15 19:43:30 -0300

    Criação da tela com a lista de todos os articles

--- app/src/main/java/com/example/aprenderdatahub/ui/screen/DataHubApp.kt
+++ app/src/main/java/com/example/aprenderdatahub/ui/screen/DataHubApp.kt
@@ -1,13 +1,21 @@
 package com.example.aprenderdatahub.ui.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Card
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -38,5 +46,32 @@ fun ArticleDetailScreen(
         Text("Categoria: ${article.category}")
         Text("Origem: ${article.source}")
         Text(article.summary)
+    }
+}
+
+@Composable
+fun ArticleList(
+    articles: List<Article>,
+    onSelect:(Article) -> Unit,
+
+){
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(articles, key = {it.id}){article->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable{(onSelect(article))}
+            ){
+                ListItem(
+                    headlineContent = {Text(article.title)},
+                    supportingContent = {Text(article.summary)},
+                    overlineContent = {Text(article.category)},
+                    trailingContent = {Text(article.source)}
+                )
+            }
+
+        }
     }
 }
